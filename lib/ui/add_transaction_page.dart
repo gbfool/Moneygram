f25b1aee794661819ed79cb37af3e6a07677ec0b
himanshuarora97 <himanshuarora3097@gmail.com>
2022-11-24 12:14:07 +0530

    added custom keyboard

--- lib/ui/add_transaction_page.dart
+++ lib/ui/add_transaction_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:moneygram/ui/keyboard/keyboard_key.dart';
 
 class AddTransactionPage extends StatefulWidget {
   const AddTransactionPage({Key? key}) : super(key: key);
@@ -19,71 +20,48 @@ class AddTransactionPage extends StatefulWidget {
 // }
 
 class _AddTransactionPageState extends State<AddTransactionPage> {
-  var _numberForm = GlobalKey<FormState>();
-  RegExp _digitRegex = RegExp("[0-9]+");
   bool isValidForm = false;
   late TextEditingController textEditingController;
+  late List<List<dynamic>> keys;
+  String amount = "";
 
   @override
   void initState() {
     textEditingController = TextEditingController();
+    keys = [
+      ['1', '2', '3'],
+      ['4', '5', '6'],
+      ['7', '8', '9'],
+      [
+        '.',
+        '0',
+        const Icon(
+          Icons.backspace,
+          color: Colors.black,
+        )
+      ],
+    ];
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          _topHeader(),
-          const SizedBox(height: 16),
-          _amountWidget(),
-          Center(
-            child: Form(
-                key: _numberForm,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      validator: (inputValue) {
-                        if (inputValue == null ||
-                            inputValue.isEmpty ||
-                            !_digitRegex.hasMatch(inputValue)) {
-                          return "Please enter number";
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      validator: (inputValue) {
-                        if (inputValue == null || inputValue.isEmpty) {
-                          return "Please Fill before";
-                        }
-                        return null;
-                      },
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          if (_numberForm.currentState?.validate() ?? false) {
-                            setState(() {
-                              isValidForm = true;
-                            });
-                          } else {
-                            setState(() {
-                              isValidForm = false;
-                            });
-                          }
-                        },
-                        child: Text("Check Number")),
-                    Text(isValidForm ? "Nice" : "Please Fix error and Submit ")
-                  ],
-                )),
-          ),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            _topHeader(),
+            const SizedBox(height: 16),
+            _amountWidget(),
+            const Spacer(),
+            ...renderKeyboard(),
+          ],
+        ),
       ),
     );
   }
 
-  Container _topHeader() {
+  Widget _topHeader() {
     return Container(
         padding: const EdgeInsets.only(left: 12, right: 12, top: 16),
         color: Colors.white,
@@ -119,8 +97,7 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
   }
 
   Widget _amountWidget() {
-    Color textColor =
-        textEditingController.text.isEmpty ? Colors.grey : Colors.black;
+    Color textColor = amount.isEmpty ? Colors.grey : Colors.black;
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -129,29 +106,93 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
           style: TextStyle(color: textColor, fontSize: 32),
         ),
         const SizedBox(width: 2),
-        IntrinsicWidth(
-          child: TextFormField(
-            style: TextStyle(
-                color: Colors.black, fontWeight: FontWeight.w600, fontSize: 32),
-            cursorColor: Colors.black,
-            controller: textEditingController,
-            keyboardType: TextInputType.numberWithOptions(decimal: true),
-            onChanged: (String value) {
-              setState(() {});
-            },
-            validator: (value) {
-              // ErrorModel? errorModel = validateField(value);
-              // logErrorEvent(errorModel);
-              // return validateField(value)?.errorMessage;
-            },
-            decoration: InputDecoration(
-              hintText: '0',
-              hintStyle: TextStyle(color: Colors.grey),
-              border: InputBorder.none,
-            ),
-          ),
-        ),
+        renderAmount(),
+        // _inputField(),
       ],
     );
+  }
+
+  Widget _inputField() {
+    return IntrinsicWidth(
+      child: TextFormField(
+        style: TextStyle(
+            color: Colors.black, fontWeight: FontWeight.w600, fontSize: 32),
+        cursorColor: Colors.black,
+        enabled: false,
+        initialValue: amount,
+        controller: textEditingController,
+        keyboardType: TextInputType.numberWithOptions(decimal: true),
+        onChanged: (String value) {
+          setState(() {});
+        },
+        validator: (value) {
+          // ErrorModel? errorModel = validateField(value);
+          // logErrorEvent(errorModel);
+          // return validateField(value)?.errorMessage;
+        },
+        decoration: InputDecoration(
+          hintText: '0',
+          hintStyle: TextStyle(color: Colors.grey),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+
+  Widget renderAmount() {
+    TextStyle style = const TextStyle(
+        color: Colors.grey, fontWeight: FontWeight.w600, fontSize: 32);
+    String display = "0";
+    if (amount.isNotEmpty) {
+      // NumberFormat f = NumberFormat('#,###');
+      display = amount;
+      // display = f.format(int.parse(amount));
+      style = style.copyWith(
+        color: Colors.black,
+      );
+    }
+    return Text(display, style: style);
+  }
+
+  onKeyTap(val) {
+    setState(() {
+      amount = amount + val;
+    });
+  }
+
+  onBackspacePress() {
+    if (amount.isEmpty) {
+      return;
+    }
+
+    setState(() {
+      amount = amount.substring(0, amount.length - 1);
+    });
+  }
+
+  renderKeyboard() {
+    return keys
+        .map(
+          (x) => Row(
+            children: x.map(
+              (y) {
+                return Expanded(
+                  child: KeyboardKey(
+                    label: y,
+                    value: y,
+                    onTap: (val) {
+                      if (val is Widget) {
+                        onBackspacePress();
+                      } else {
+                        onKeyTap(val);
+                      }
+                    },
+                  ),
+                );
+              },
+            ).toList(),
+          ),
+        )
+        .toList();
   }
 }
