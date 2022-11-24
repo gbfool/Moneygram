84f6247eebb6ad986724d63ccad821cc45c6502d
himanshuarora97 <himanshuarora3097@gmail.com>
2022-11-24 18:47:34 +0530

    moved actions widget

--- lib/ui/add_transaction/actions_widget.dart
+++ lib/ui/add_transaction/actions_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ActionsWidget extends StatefulWidget {
   const ActionsWidget({Key? key}) : super(key: key);
@@ -10,6 +11,57 @@ class ActionsWidget extends StatefulWidget {
 class _ActionsWidgetState extends State<ActionsWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.only(top: 12, left: 12, right: 12),
+      child: Column(
+        children: [
+          Row(children: [
+            Text("Today"),
+            const SizedBox(width: 4),
+            const Text(
+              '\u2022',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(width: 4),
+            Expanded(child: _inputField())
+          ]),
+          SizedBox(height: 6),
+          Divider(height: 1, thickness: 0),
+          SizedBox(height: 12),
+          _accountCategory(),
+          SizedBox(height: 12),
+          Divider(height: 1, thickness: 0)
+        ],
+      ),
+    );
+  }
+
+  Widget _inputField() {
+    return TextField(
+      cursorColor: Colors.black,
+      decoration:
+          InputDecoration(border: InputBorder.none, hintText: "Add notes"),
+    );
+  }
+
+  Widget _accountCategory() {
+    return Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+      Text("💳", style: GoogleFonts.notoEmoji(fontSize: 24)),
+      SizedBox(width: 4),
+      Text(
+        "Credit Card",
+        style: TextStyle(fontSize: 16),
+      ),
+      SizedBox(width: 12),
+      Icon(Icons.arrow_forward),
+      SizedBox(width: 12),
+      Text("🍿", style: GoogleFonts.notoEmoji(fontSize: 24)),
+      SizedBox(width: 4),
+      Text(
+        "Enterntainment",
+        style: TextStyle(fontSize: 16),
+      ),
+    ]);
   }
 }
--- lib/ui/add_transaction/add_transaction_page.dart
+++ lib/ui/add_transaction/add_transaction_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:moneygram/ui/add_transaction/actions_widget.dart';
 import 'package:moneygram/ui/add_transaction/amount_widget.dart';
 import 'package:moneygram/ui/add_transaction/header_widget.dart';
 import 'package:moneygram/ui/add_transaction/keyboard_widget.dart';
 import 'package:moneygram/ui/add_transaction/save_button.dart';
-import 'package:moneygram/ui/keyboard/keyboard_key.dart';
 
 class AddTransactionPage extends StatefulWidget {
   const AddTransactionPage({Key? key}) : super(key: key);
@@ -37,7 +36,7 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
             children: [
               HeaderWidget(),
               Expanded(child: AmountWidget(amount: amount)),
-              _renderActions(),
+              ActionsWidget(),
               KeyboardWidget(
                   onBackPress: onBackspacePress, onKeyPress: onKeyTap),
               SaveButton()
@@ -62,60 +61,5 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
     setState(() {
       amount = amount.substring(0, amount.length - 1);
     });
-  }
-
-  _renderActions() {
-    return Container(
-      padding: const EdgeInsets.only(top: 12, left: 12, right: 12),
-      child: Column(
-        children: [
-          Row(children: [
-            Text("Today"),
-            const SizedBox(width: 4),
-            const Text(
-              '\u2022',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(width: 4),
-            Expanded(child: _inputField())
-          ]),
-          SizedBox(height: 6),
-          Divider(height: 1, thickness: 0),
-          SizedBox(height: 12),
-          _accountCategory(),
-          SizedBox(height: 12),
-          Divider(height: 1, thickness: 0)
-        ],
-      ),
-    );
-  }
-
-  Widget _accountCategory() {
-    return Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-      Text("💳", style: GoogleFonts.notoEmoji(fontSize: 24)),
-      SizedBox(width: 4),
-      Text(
-        "Credit Card",
-        style: GoogleFonts.pacifico(fontSize: 16),
-      ),
-      SizedBox(width: 12),
-      Icon(Icons.arrow_forward),
-      SizedBox(width: 12),
-      Text("🍿", style: GoogleFonts.notoEmoji(fontSize: 24)),
-      SizedBox(width: 4),
-      Text(
-        "Enterntainment",
-        style: TextStyle(fontSize: 16),
-      ),
-    ]);
-  }
-
-  Widget _inputField() {
-    return TextField(
-      cursorColor: Colors.black,
-      decoration:
-          InputDecoration(border: InputBorder.none, hintText: "Add notes"),
-    );
   }
 }
