967102d8f1decea7d70b88866d994cf10f639778
humblefoool <humbledevs@gmail.com>
2023-10-12 15:49:07 +0530

    renamed name

--- lib/main.dart
+++ lib/main.dart
@@ -137,7 +137,7 @@ class _MyAppState extends State<MyApp> {
             ),
             themeMode: themeMode,
             navigatorKey: navigatorKey,
-            title: 'Money',
+            title: 'Bucky',
             home: BottomNavigationState(),
           );
         });
